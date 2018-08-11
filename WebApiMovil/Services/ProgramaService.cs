60fb8669b4166f22f1f1687a22f0578af599a92e
Manuel Llerena <mllerenag@gmail.com>
2018-08-11 13:49:52 -0500

    Update ProgramaService.cs

--- WebApiMovil/Services/ProgramaService.cs
+++ WebApiMovil/Services/ProgramaService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,12 +9,116 @@ namespace WebApiMovil.Services
 {
     public class ProgramaService
     {
+
         private ProgramaBL programaBL;
 
         public ProgramaService()
         {
             programaBL = new ProgramaBL();
+ 
         }
+
+        public ResultadoLogin Login(ResultadoLogin obj)
+        {
+            try
+            {
+                return programaBL.Login(obj);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public List<Programa_Response> BuscarProgramas(Programa_Request obj)
+        {
+            try
+            {
+                return programaBL.BuscarProgramas(obj);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+         public PopUp_Programa_Response DatosPopUp(Programa_Request obj)
+        {
+            try
+            {
+                return programaBL.DatosPopUp(obj);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Proyecto_Response> obtenerProyectosDisponibles(Proyecto_Request obj)
+        {
+            try
+            {
+                return programaBL.obtenerProyectosDisponibles(obj);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Programa_Proyectos> AsociarProyecto(AsociarProyecto_Request obj)
+        {
+            try
+            {
+                return programaBL.AsociarProyecto(obj);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public List<Programa_Proyectos> DesasociarProyecto(DesAsociarProyecto_Request obj)
+        {
+            try
+            {
+                return programaBL.DesasociarProyecto(obj);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+        public EliminarPrograma_Response EliminarPrograma(EliminarPrograma_Request obj)
+        {
+            try
+            {
+                return programaBL.EliminarPrograma(obj);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public ActualizarPrograma_Response ActualizarPrograma(ActualizarPrograma_Request obj)
+        {
+            try
+            {
+                return programaBL.ActualizarPrograma(obj);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
 
     }
 }
